93065a6b6a9795e0cc2d9fcf68c6ec5cc1d466f7
Ejup Pllashniku <ejup.pllashniku@gmail.com>
2024-08-01 20:38:04 +0200

    add some responsive changes

--- scripts/app.js
+++ scripts/app.js
@@ -28,13 +28,13 @@ function currentInfo(name, temp, icon) {
   const currentInfo = document.getElementById("currentInfo");
   currentInfo.innerHTML = `
   <div class="w-80 flex flex-col justify-between">
-    <h1 class="text-white text-2xl font-semibold">
+    <h1 class="text-white text-2xl sm:text-3xl md:text-5xl font-semibold">
       ${name}
     </h1>
     <p class="text-white text-3xl font-medium" >${temp}°</p>
   </div>
   <div class='my-auto'>
-    <img src="${icon}" alt="Weather Icon" class='h-28 w-28 md:h-full md:w-48 object-contain ite ' />
+    <img src="${icon}" alt="Weather Icon" class='object-contain w-32 h-32 md:w-36 md:h-36  ' />
   </div>
   `;
 }
@@ -58,11 +58,11 @@ function weeklyInfo(data) {
           <div class='py-4 flex justify-between '>
             <div class='flex pr-2'>
               <img class='object-contain' src="${day.day.condition.icon}" alt="${day.day.condition.text}" />
-              <p class='text-white text-opacity-70 place-self-center'>${day.day.condition.text}</p>
+              <p class='text-white text-base md:text-xl  text-opacity-70 place-self-center'>${day.day.condition.text}</p>
             </div>
             <div class='place-content-center'>
-              <span class='text-white text-opacity-80 place-self-center'>${wholeMaxTemp}°</span><span class='text-white text-opacity-50 place-self-center'> /${wholeMinTem}°</span>
-              <p class="text-white text-opacity-50 place-self-center">${dayName}</p>
+              <span class='text-white text-base md:text-xl text-opacity-80 place-self-center'>${wholeMaxTemp}°</span><span class='text-white text-base md:text-xl text-opacity-50 place-self-center'> /${wholeMinTem}°</span>
+              <p class="text-white text-base md:text-xl text-opacity-50 place-self-center">${dayName}</p>
             </div>
           </div>`;
     })
@@ -92,11 +92,13 @@ function hourlyData(data) {
 
       return `
         <div class='flex flex-col items-center p-2'>
-          <p class='text-white text-opacity-50 text-center '>${formatHour(
+          <p class='text-white text-base md:text-xl text-opacity-50 text-center '>${formatHour(
             hourOnly
           )}</p>
           <img src="${hour.condition.icon}" alt="${hour.condition.text}" />
-          <p class='text-white text-opacity-90 text-center'>${hour.temp_c}°</p>
+          <p class='text-white text-base md:text-xl text-opacity-90 text-center'>${
+            hour.temp_c
+          }°</p>
         </div>
        `;
     })
@@ -105,7 +107,6 @@ function hourlyData(data) {
 
 function airConditions(data) {
   const airConditions = document.getElementById("airConditions");
-
   const airData = data.current;
 
   airConditions.innerHTML = `
@@ -114,28 +115,28 @@ function airConditions(data) {
       <i class="fa-solid fa-temperature-half mr-2"></i>
       Real Feel
     </span>
-    <h2 class="text-base pt-2">${airData.heatindex_c}°C</h2>
+    <h2 class="text-base md:text-xl pt-2">${airData.heatindex_c}°C</h2>
   </div>
-  <div class="flex flex-col items-center">
-    <span class="text-white text-base text-opacity-50 flex items-center">
+  <div class="flex flex-col items-center ">
+    <span class="text-white text-base md:text-xl text-opacity-50 flex items-center text-nowrap ">
       <i class="fa-solid fa-cloud-showers-heavy mr-2"></i>
       Chance of rain
     </span>
-    <h2 class="text-base pt-2">${airData.precip_mm} mm</h2>
+    <h2 class="text-base md:text-xl pt-2">${airData.precip_mm} mm</h2>
   </div>
   <div class="flex flex-col items-center">
-    <span class="text-white text-base text-opacity-50 flex items-center">
+    <span class="text-white text-base md:text-xl text-opacity-50 flex items-center">
       <i class="fa-solid fa-sun mr-2"></i>
       UV Index
     </span>
-    <h2 class="text-base pt-2">${airData.uv}</h2>
+    <h2 class="text-base md:text-xl pt-2">${airData.uv}</h2>
   </div>
   <div class="flex flex-col items-center">
-    <span class="text-white text-base text-opacity-50 flex items-center">
+    <span class="text-white text-base md:text-xl text-opacity-50 flex items-center">
       <i class="fa-solid fa-wind mr-2"></i>
       Wind
     </span>
-    <h2 class="text-base pt-2">${airData.wind_kph} km/h</h2>
+    <h2 class="text-base md:text-xl pt-2">${airData.wind_kph} km/h</h2>
   </div>
 `;
 }
